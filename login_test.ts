38b7f014aa80eeb4819ecaf66ed547bc988a3650
klxfe <jaguar2374@gmail.com>
2022-12-14 13:45:59 +0100

    sec commit

--- login_test.ts
+++ login_test.ts
@@ -1,6 +1,16 @@
-Feature('login');
+Feature('login');   
 
-Scenario('Test Redirection', async ({ I }) => {
+
+
+Scenario('Test Log In', async ({ I }) => {
     I.amOnPage('/muj/prihlaseni');
+    I.wait(3);
+    if(I.see('Vodafone Cookies')){
+        I.click('Povolit nezbytné');
+    }
     I.see('Přihlášení do Můj Vodafone');
+    I.fillField('Telefonní číslo, e-mail, číslo služby', '773088835');
+    I.fillField('Napište své heslo', '237420');
+    I.click('Přihlásit se');
+    I.wait(5);
 });
